8bab79578b1fd06e8ac5bbdbee36f39ebab1b212
Iztok Oder <iztok@celtra.com>
2017-03-12 16:30:00 +0100

    Added legend group to options, which is build based on current status of container.

--- options.js
+++ options.js
@@ -5,4 +5,10 @@ var Options = function (container, color, minValue, maxValue, step, radius) {
 	this.maxValue  = maxValue;
 	this.step      = step;
 	this.radius    = radius;
+
+	this.legendGroup = container.getElementsByTagName('g')[0];
+	if(!this.legendGroup) {
+		this.legendGroup = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+		this.container.appendChild(this.legendGroup);
+	}
 }
