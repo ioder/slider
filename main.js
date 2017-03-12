8e1d42dd5e16160c94301d7ab3b7cfddd5f75634
Iztok Oder <iztok@celtra.com>
2017-03-12 16:29:16 +0100

    Added meta tags to index.html. Added more sliders.

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-window.onload = function() {
-	var svgElement = document.getElementById("slider-svg");
+var svgElement = document.getElementById("slider-svg");
 
-	var sliderOptions1 = new Options(svgElement, "red", 0, 160, 10, 50);
-	var slider1 = new Slider(sliderOptions1);  
-};
+var sliderOptions1 = new Options(svgElement, "red", -400, 160, 20, 50);
+var slider1 = new Slider(sliderOptions1);
+var sliderOptions2 = new Options(svgElement, "green", 20, 250, 10, 100);
+var slider2 = new Slider(sliderOptions2);
+var sliderOptions3 = new Options(svgElement, "yellow", -300, 400, 7, 150);
+var slider3 = new Slider(sliderOptions3);
+var sliderOptions4 = new Options(svgElement, "blue", 0, 1400, 4, 190);
+var slider4 = new Slider(sliderOptions4);
